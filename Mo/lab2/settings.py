1c62f4ffdf979acf4100501bd2c52dc3425246be
Mytyai <shetkindima@yandex.ru>
2024-12-24 12:50:05 -0800

    ver 1

---
+++ Mo/lab2/settings.py
@@ -0,0 +1,22 @@
+
+
+class Settings():
+    """Класс для хранения настроек игры."""
+
+    def __init__(self):
+        """Инициализирует настройки игры."""
+        
+        self.screen_widht = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+        self.ship_speed = 1.5
+
+        self.bullet_speed = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 3
+
+        self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        self.fleet_direction = 1
